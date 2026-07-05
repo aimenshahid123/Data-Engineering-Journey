c5bc353e36a94040e2870509323b5023cfaca8f1
aimenshahid123 <116283358+aimenshahid123@users.noreply.github.com>
2026-07-05 22:48:49 +0500

    variables,input, output and first exercise

---
+++ 01-Python/exercise_input.py
@@ -0,0 +1,36 @@
+
+
+# =================================
+#        STUDENT PROFILE
+# =================================
+# Name       : Aimen
+# Age        : 23
+# University : COMSATS
+# Degree     : BS Computer Science
+# CGPA       : 2.33
+# Dream Job  : Data Engineer
+# =================================
+
+
+
+name = input(" Enter your Name : ")
+age = int(input(" Enter your Age : "))
+university = input(" Enter your University : ")
+degree = input(" Enter your Degree : ")
+cgpa = float(input(" Enter your CGPA : "))
+dreamjob = input(" Enter your Dream Job : ")
+
+print("="*40)
+print("STUDENT PROFILE".center(40))
+print("="*40)
+print(f"Name       : {name}")
+print(f"Age        : {age}")
+print(f"University : {university}")
+print(f"Degree     : {degree}")
+print(f"CGPA       : {cgpa}")
+print(f"Dream Job  : {dreamjob}")
+print("=================================")
+
+
+
+
